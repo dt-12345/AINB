5c50ea0030c13850371efd6f6833e55019876df3
dt-12345 <damagetesting@proton.me>
2025-08-26 21:30:06 -0400

    oops didn't mean to comment out the first test case

--- tests/test.py
+++ tests/test.py
@@ -12,32 +12,32 @@ def fix_path(path: str) -> str:
 
 INPUT_DIRECTORY: str = fix_path("data")
 
-# class RoundtripTest(unittest.TestCase):
-#     def test_json_roundtrip(self) -> None:
-#         ainb.set_splatoon3()
-#         for file in os.listdir(INPUT_DIRECTORY):
-#             if not os.path.isfile(os.path.join(INPUT_DIRECTORY, file)):
-#                 continue
-#             try:
-#                 print(file)
-#                 orig: ainb.AINB = ainb.AINB.from_file(os.path.join(INPUT_DIRECTORY, file), read_only=False)
-#                 new: ainb.AINB = ainb.AINB.from_json_text(orig.to_json())
-#             except Exception as e:
-#                 self.fail(f"{file} failed: {e.args}")
-#             self.assertDictEqual(orig.as_dict(), new.as_dict(), f"{file} is mismatching")
+class RoundtripTest(unittest.TestCase):
+    def test_json_roundtrip(self) -> None:
+        ainb.set_splatoon3()
+        for file in os.listdir(INPUT_DIRECTORY):
+            if not os.path.isfile(os.path.join(INPUT_DIRECTORY, file)):
+                continue
+            try:
+                print(file)
+                orig: ainb.AINB = ainb.AINB.from_file(os.path.join(INPUT_DIRECTORY, file), read_only=False)
+                new: ainb.AINB = ainb.AINB.from_json_text(orig.to_json())
+            except Exception as e:
+                self.fail(f"{file} failed: {e.args}")
+            self.assertDictEqual(orig.as_dict(), new.as_dict(), f"{file} is mismatching")
     
-#     def test_ainb_roundtrip(self) -> None:
-#         ainb.set_splatoon3()
-#         for file in os.listdir(INPUT_DIRECTORY):
-#             if not os.path.isfile(os.path.join(INPUT_DIRECTORY, file)):
-#                 continue
-#             try:
-#                 print(file)
-#                 orig: ainb.AINB = ainb.AINB.from_file(os.path.join(INPUT_DIRECTORY, file), read_only=False)
-#                 new: ainb.AINB = ainb.AINB.from_binary(orig.to_binary())
-#             except Exception as e:
-#                 self.fail(f"{file} failed: {e.args}")
-#             self.assertDictEqual(orig.as_dict(), new.as_dict(), f"{file} is mismatching")
+    def test_ainb_roundtrip(self) -> None:
+        ainb.set_splatoon3()
+        for file in os.listdir(INPUT_DIRECTORY):
+            if not os.path.isfile(os.path.join(INPUT_DIRECTORY, file)):
+                continue
+            try:
+                print(file)
+                orig: ainb.AINB = ainb.AINB.from_file(os.path.join(INPUT_DIRECTORY, file), read_only=False)
+                new: ainb.AINB = ainb.AINB.from_binary(orig.to_binary())
+            except Exception as e:
+                self.fail(f"{file} failed: {e.args}")
+            self.assertDictEqual(orig.as_dict(), new.as_dict(), f"{file} is mismatching")
 
 class GraphTest(unittest.TestCase):
     def test(self) -> None:
